2b38e510156d204dedd7562f0139fd61da232f8c
nafiz <nafizimrose.official@gmail.com>
2024-05-20 19:47:32 +0600

    first commit

---
+++ first_C++.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+
+int main(){
+    int a;
+    float b;
+    char c;
+    double d;
+    cin >>b;
+    cout<<b;
+
+    
+    return 0;
+
+
+}
